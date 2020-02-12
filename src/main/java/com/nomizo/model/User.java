e4330e56f0a1a6394e41e1247878cd7fb93b01e9
nomizo-iox <nomizo.iox@gmail.com>
2020-02-11 20:50:23 -0600

    Added SonarCloud and corrected issue with annotation @ManyToAny to @ManyToMany.

--- src/main/java/com/nomizo/model/User.java
+++ src/main/java/com/nomizo/model/User.java
@@ -43,7 +43,7 @@ public class User {
     @Size(max = 40)
     private String password;
 
-    @ManyToAny(fetch = FetchType.LAZY)
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "user_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
