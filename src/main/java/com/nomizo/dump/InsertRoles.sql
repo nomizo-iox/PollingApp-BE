7e6810cdf4d196fa861d12d2d2d31221ec29c034
nomizo-iox <nomizo.iox@gmail.com>
2020-02-13 00:47:56 -0600

    added some new files.

--- src/main/java/com/nomizo/dump/InsertRoles.sql
+++ src/main/java/com/nomizo/dump/InsertRoles.sql
@@ -2,3 +2,4 @@ use pollingdb;
 
 INSERT INTO roles(name) VALUES('ROLE_USER');
 INSERT INTO roles(name) VALUES('ROLE_ADMIN');
+INSERT INTO roles(name) VALUES('ROLE_THANOS');
--- src/main/java/com/nomizo/model/RoleName.java
+++ src/main/java/com/nomizo/model/RoleName.java
@@ -3,5 +3,5 @@ package com.nomizo.model;
 public enum RoleName {
     ROLE_USER,
     ROLE_ADMIN,
-    ROLE_MANAGER
+    ROLE_THANOS
 }
