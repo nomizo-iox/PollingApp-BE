d750b8063f278f101da3457d8bc9ce7ca1d65d30
nomizo-iox <nomizo.iox@gmail.com>
2020-02-13 00:12:59 -0600

    changes to test and added an sql.

---
+++ src/test/java/com/nomizo/db/UserRepositoryTest.java
@@ -0,0 +1,32 @@
+package com.nomizo.db;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserRepositoryTest {
+
+    @Test
+    void findByEmail() {
+    }
+
+    @Test
+    void findByUsernameOrEmail() {
+    }
+
+    @Test
+    void findByIdIn() {
+    }
+
+    @Test
+    void findByUsername() {
+    }
+
+    @Test
+    void existsByUsername() {
+    }
+
+    @Test
+    void existsByEmail() {
+    }
+}
